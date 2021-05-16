365241b749e96ea35e18424b31832070c6b34f14
Kate <katerina_gutich@mail.ru>
2021-05-16 12:38:39 +0300

    Update expenses object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
 
@@ -6,24 +6,24 @@ function App() {
         {
             id: 'e1',
             title: 'Shopping',
-            amount: 94.12,
+            amount: 194.12,
             date: new Date(2020, 7, 14),
         },
         {
             id: 'e2',
-            title: 'New TV',
-            amount: 799.49,
+            title: 'English Courses',
+            amount: 59.29,
             date: new Date(2021, 2, 12)
         },
         {
             id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
+            title: 'Taxi',
+            amount: 14.67,
             date: new Date(2021, 2, 28),
         },
         {
             id: 'e4',
-            title: 'New Desk (Wooden)',
+            title: 'New Laptop',
             amount: 450,
             date: new Date(2021, 5, 12),
         },
