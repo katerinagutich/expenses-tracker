3d3528024f62931f9f3063bcf3b0d889515d560e
Kate <katerina_gutich@mail.ru>
2021-05-17 18:38:42 +0300

    Add cancel button to the form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = ({onSaveExpenseData}) => {
+const ExpenseForm = ({onSaveExpenseData, onCancel}) => {
 
     const [enteredTitle, setEnteredTitle] = useState("");
     const [enteredDate, setEnteredDate] = useState("");
@@ -23,7 +23,7 @@ const ExpenseForm = ({onSaveExpenseData}) => {
         event.preventDefault();
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         };
         onSaveExpenseData(expenseData);
@@ -69,6 +69,7 @@ const ExpenseForm = ({onSaveExpenseData}) => {
                 </div>
             </div>
             <div className="new-expense__actions">
+                <button onClick={onCancel}>Cancel</button>
                 <button type="submit">Submit</button>
             </div>
         </form>
