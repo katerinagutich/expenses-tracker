0b91eb5579661528accd2ff8f878fda908dda02b
Kate <katerina_gutich@mail.ru>
2021-05-17 17:07:05 +0300

    Extract initial expenses from App component, add map method to Expenses to render ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
+import React, {useState} from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const INITIAL_EXPENSES = [
+    {
+        id: 'e1',
+        title: 'Shopping',
+        amount: 194.12,
+        date: new Date(2020, 7, 14),
+    },
+    {
+        id: 'e2',
+        title: 'English Courses',
+        amount: 59.29,
+        date: new Date(2021, 2, 12)
+    },
+    {
+        id: 'e3',
+        title: 'Taxi',
+        amount: 14.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: 'e4',
+        title: 'New Laptop',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+];
+
 function App() {
 
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Shopping',
-            amount: 194.12,
-            date: new Date(2020, 7, 14),
-        },
-        {
-            id: 'e2',
-            title: 'English Courses',
-            amount: 59.29,
-            date: new Date(2021, 2, 12)
-        },
-        {
-            id: 'e3',
-            title: 'Taxi',
-            amount: 14.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Laptop',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ];
+    const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
     const addExpenseHandler = expense => {
-        console.log(expense);
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses];
+        });
     };
 
     return (
