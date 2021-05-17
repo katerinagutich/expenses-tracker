7a1bc73876399d7e92722fbc5a2483b2424a9c33
Kate <katerina_gutich@mail.ru>
2021-05-17 15:48:23 +0300

    Lift the state up from ExpenseForm to App component

--- src/App.js
+++ src/App.js
@@ -30,9 +30,13 @@ function App() {
         },
     ];
 
+    const addExpenseHandler = expense => {
+        console.log(expense);
+    };
+
     return (
         <div>
-            <NewExpense/>
+            <NewExpense onAddExpense={addExpenseHandler}/>
             <Expenses items={expenses}/>
         </div>
 
