c1a392b761a50f565cac01f6342ad431e2598c89
Kate <katerina_gutich@mail.ru>
2021-05-17 16:25:36 +0300

    Create ExpenseFilter component and add styles to it

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,23 @@
+import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 import './Expenses.css';
 
 function Expenses(props) {
+
+    const [filteredYear, setFilteredYear] = useState("2020")
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
+
     return (
         <Card className="expenses">
+            <ExpenseFilter
+                selected={filteredYear}
+                onFilterChange={filterChangeHandler}
+            />
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
