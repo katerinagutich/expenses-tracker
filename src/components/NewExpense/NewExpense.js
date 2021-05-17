f9fdec7687c154d218d57946efbd00b1cbc3ebe0
Kate <katerina_gutich@mail.ru>
2021-05-17 18:39:15 +0300

    Add state for editing the form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
+import React, {useState} from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = ({onAddExpense}) => {
+
+    const [isEditing, setIsEditing] = useState(false);
 
     const saveExpenseDataHandler = enteredExpenseData => {
         const expenseData = {
@@ -9,11 +12,28 @@ const NewExpense = ({onAddExpense}) => {
             id: Math.random().toString()
         };
         onAddExpense(expenseData);
+        setIsEditing(false);
+    };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
     };
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add New Expense</button>
+            )}
+            {isEditing && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={stopEditingHandler}
+                />
+            )}
         </div>
     );
 }
